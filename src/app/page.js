e309192cad64a9ee11527e6eea1b9a713d51d0f7
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-05-25 13:09:29 +0200

    more skeleton added

--- src/app/page.js
+++ src/app/page.js
@@ -64,16 +64,23 @@ export default function Home() {
 				</aside>
 				<section className='articles'>
 					<ArticleCard
+						// TODO: do thhe article alts
+						articleImage='image-retro-pcs'
+						articleAlt=''
 						articleNumber='01'
 						title='Reviving Retro PCs'
 						text='What happens when old PCs are given modern upgrades?'
 					/>
 					<ArticleCard
+						articleImage='image-top-laptops'
+						articleAlt=''
 						articleNumber='02'
 						title='Top 10 Laptops of 2022'
 						text='Our best picks for various needs and budgets.'
 					/>
 					<ArticleCard
+						articleImage='image-gaming-growth'
+						articleAlt=''
 						articleNumber='03'
 						title='The Growth of Gaming'
 						text='How the pandemic has sparked fresh opportunities.'
