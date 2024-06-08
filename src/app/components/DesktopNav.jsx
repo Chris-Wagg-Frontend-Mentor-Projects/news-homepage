a1a0d94c0cbed65e8a12ed9209bf902eed552efb
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-06-08 10:55:04 +0200

    mobile nav changed to popover. all useEffect and useState stuff removed

--- src/app/components/DesktopNav.jsx
+++ src/app/components/DesktopNav.jsx
@@ -1,48 +1,9 @@
 'use client'
 
 import Link from 'next/link'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 export default function DesktopNav() {
-	const [isOpen, setIsOpen] = useState()
-	const [isClosed, setIsClosed] = useState()
-
-	useEffect(() => {
-		let openMenuBtn = document.getElementById('openMenuBtn')
-		let mobileNav = document.getElementById('mobileNav')
-		let closeMenuBtn = document.getElementById('closeMenuBtn')
-
-		function openMobileNav() {
-			console.log('open button pressed')
-			mobileNav.style.display = 'flex'
-			openMenuBtn.style.display = 'none'
-			closeMenuBtn.style.display = 'flex'
-			setIsOpen(true)
-			setIsClosed(false)
-		}
-
-		if (isOpen === true) {
-			openMobileNav()
-		}
-	}, [isOpen])
-
-	useEffect(() => {
-		let openMenuBtn = document.getElementById('openMenuBtn')
-		let mobileNav = document.getElementById('mobileNav')
-		let closeMenuBtn = document.getElementById('closeMenuBtn')
-
-		function closeMobileNav() {
-			console.log('close button pressed')
-			mobileNav.style.display = 'none'
-			closeMenuBtn.style.display = 'none'
-			setIsOpen(false)
-			setIsClosed(true)
-		}
-
-		if (isClosed === true) {
-			closeMobileNav()
-		}
-	}, [isClosed])
 	return (
 		<>
 			<nav className='nav-bar--desktop'>
@@ -61,19 +22,21 @@ export default function DesktopNav() {
 				<Link href={'/'} className='nav-bar__button--desktop'>
 					Categories
 				</Link>
+
 				<button
 					className='hamburger-btn'
-					id='openMenuBtn'
-					onClick={() => setIsOpen(true)}>
+					popovertarget='popoverMobileNav'>
 					<img src='icon-menu.svg' alt='open menu' />
 				</button>
 			</nav>
 
-			<nav className='nav-bar--mobile' id='mobileNav'>
+			<nav
+				className='nav-bar--mobile'
+				popover='auto'
+				id='popoverMobileNav'>
 				<button
 					className='close-button'
-					id='closeMenuBtn'
-					onClick={() => setIsClosed(true)}>
+					popovertarget='popoverMobileNav'>
 					<img src='icon-menu-close.svg' alt='close menu' />
 				</button>
 
