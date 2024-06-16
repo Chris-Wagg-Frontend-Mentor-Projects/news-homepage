e70737429f289609e881c248335cd50265ff943d
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-06-16 13:41:55 +0200

    grid layouts and spacings fixed

--- src/app/components/ArticleCard.jsx
+++ src/app/components/ArticleCard.jsx
@@ -2,8 +2,12 @@ export default function ArticleCard(props) {
 	const { articleImage, articleAlt, articleNumber, title, text } = props
 	return (
 		<article className='article-card'>
-			<img src={`./${articleImage}.jpg`} alt={articleAlt} />
-			<div>
+			<img
+				src={`./${articleImage}.jpg`}
+				alt={articleAlt}
+				className='article-card__image'
+			/>
+			<div className='article-card__text-container'>
 				<h2 className='article-card__number'>{articleNumber}</h2>
 				<h3 className='article-card__title'>{title}</h3>
 				<p className='article-card__text'>{text}</p>
